661dc7d55d773f058a3c9051956b2d69bc9be6f7
Elliott Joo <elliott.joo@oracle.com>
2017-11-04 14:06:25 -0700

    added token

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ var messagebody = {
 }
 
 //**When you first connect, tell dashbot and save the bot and team locally
-request('https://slack.com/api/rtm.start?token=' + process.env.SLACK_BOT_TOKEN, function(error, response) {
+request('https://slack.com/api/rtm.start?token=' + process.env.xoxb-267119409108-O523dJdn6nptZyWbDzSDFFeC, function(error, response) {
   const parsedData = JSON.parse(response.body);
 
   // Tell dashbot when you connect.
